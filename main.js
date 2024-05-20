478e784004be1298a0ea25dc1a7ab0109679f6af
Andrea Laserna <andrealaserna1@gmail.com>
2024-05-20 22:52:57 +0800

    html and js initialized

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+ctx.fillStyle = 'skyblue';
+ctx.fillRect(0, 0, 50, 50);
