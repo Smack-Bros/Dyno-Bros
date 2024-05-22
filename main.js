cb025c91d0bc027c03e80228fd6ac10eaa7f45b4
Andrea Laserna <andrealaserna1@gmail.com>
2024-05-23 00:05:04 +0800

    added css file and shapes in main.js

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
 
-ctx.fillStyle = 'skyblue';
-ctx.fillRect(0, 0, 50, 50);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+canvas.style.background = 'skyblue';
+
+ctx.fillStyle = 'pink';
+ctx.fillRect(0, 0, 100, 100);
+
+ctx.fillStyle = 'violet';
+ctx.fillRect(100, 100, 100, 100);
+
+ctx.beginPath();
+ctx.strokeStyle = 'yellow';
+ctx.lineWidth = 10;
+ctx.arc(100, 100, 50, 0, Math.PI * 2, false);
+ctx.stroke();
+ctx.closePath();
